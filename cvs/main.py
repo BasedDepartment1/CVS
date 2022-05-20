2016969d555067a4c3df06995b4f0b5bfa965dd1
Vladimir_Pestov <Vladimir.Pestov@urfu.me>
2022-05-20 21:12:51 +0500

    Refactored main.py

--- cvs/main.py
+++ cvs/main.py
@@ -3,6 +3,51 @@ import time
 import logging
 
 
+# All functions are just for the sake of demonstration how main() will work
+def init(*args):
+    print("init cool")
+
+
+def add(*args):
+    if len(args) != 1:
+        logging.error("Incorrect format: number of arguments must be 1 "
+                      + f"but was: {len(args)}")
+        sys.exit(2)
+
+    print("adding...")
+    time.sleep(1)
+
+
+def commit(*args):
+    if len(args) == 2 and args[0] == "-m":
+        print(args[1])
+    elif args == ([],):
+        print("commit no message")
+    else:
+        logging.error("Number of arguments must be 2 or 0 "
+                      + f"but was: {len(args)}")
+        sys.exit(3)
+
+
+def reset(*args):
+    print("reset")
+
+
+def log(*args):
+    print("1: ya")
+    print("2: ebal")
+    print("3: sobaky")
+
+
+COMMANDS = {
+    "init": init,
+    "add": add,
+    "commit": commit,
+    "reset": reset,
+    "log": log,
+}
+
+
 def main():
     fmt = "[%(levelname)s] %(message)s"
     logging.basicConfig(format=fmt, level=logging.ERROR)
@@ -10,30 +55,12 @@ def main():
     if len(sys.argv) == 1:
         logging.error("No args")
         sys.exit(1)
-    match sys.argv[1]:
-        case "init":
-            print("init cool")
-        case "add":
-            if len(sys.argv) == 2:
-                logging.error("Nothing to add")
-                sys.exit(2)
-            print("adding...")
-            time.sleep(5)
-            sys.exit(0)
-        case "commit":
-            if len(sys.argv) == 4 and sys.argv[2] == "-m":
-                print(sys.argv[3])
-            else:
-                print("commit no message")
-        case "reset":
-            print("reset")
-        case "log":
-            print("1: ya")
-            print("2: ebal")
-            print("3: sobaky")
-        case _:
-            logging.error("No such command")
-            sys.exit(3)
+
+    try:
+        COMMANDS[sys.argv[1]](*sys.argv[2:])
+    except KeyError:
+        logging.error("No such command")
+        sys.exit(4)
 
 
 if __name__ == '__main__':
